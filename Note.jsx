9c59a283e50a1e8b93deccd5a09c28e7d757e2b4
Hassan Atoui <hasanatwi00@gmail.com>
2026-05-08 09:22:11 +0300

    Updated the project

--- Note.jsx
+++ Note.jsx
@@ -32,7 +32,7 @@ function Note({ isValid2, nameOfTheUser }) {
           <h1>
             FRESHLY ROASTED
             <br />
-            COFFEE
+            COFFEE trying something
           </h1>
           <p>
             Discover the flavor of our freshly roasted coffee.
