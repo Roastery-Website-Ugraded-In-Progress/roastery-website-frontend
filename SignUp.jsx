9b52d367bb24c801d3590a2dfd2ee9fdce8ca59c
hasanatwi <127450179+hasanatwi@users.noreply.github.com>
2026-07-29 18:34:14 +0300

    Update SignUp.jsx

--- SignUp.jsx
+++ SignUp.jsx
@@ -22,7 +22,7 @@ function SignUp({ isValid2, setIsValid2, setNameOfTheUser2, setEmail2 }) {
       if(!validEmail || !validPassword)
         return;
       setIsValid2(true);
-      const response=await fetch(`http://localhost:3000/signUp`,
+      const response=await fetch(`https://roastery-website-backend-2.onrender.com/signUp`,
         {
           method: "POST",
           headers: {"Content-Type":"application/json"},
